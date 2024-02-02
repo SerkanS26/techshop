c2d0659067fde5acd0d7fb6fb33ac4fc99ea3a9c
SerkanS26 <s.safran84@gmail>
2024-02-02 14:42:53 +0100

    User Email & Password Validation & JWT as HTTP-Only Cookie

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,26 +8,33 @@ const notFound = (req, res, next) => {
 };
 
 //error handler middleware
+
+// const errorHandler = (err, req, res, next) => {
+//   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+//   let message = err.message;
+
+//   //check for Mongoose bad ObjectId
+//   if (err.name === "CastError" && err.kind === "ObjectId") {
+//     message = "Resource not found";
+//     statusCode = 404;
+//     res.status(statusCode).json({
+//       message,
+//       stack: process.env.NODE_ENV === "production" ? "🍰" : err.stack,
+//     });
+//   }
+// };
+
 const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  let message = err.message;
+  console.log("errorHandler middleware");
+  const statusCode = res.statusCode || 500;
 
-  //check for Mongoose bad ObjectId
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = "Resource not found";
-    statusCode = 404;
-    res.status(statusCode).json({
-      message,
-      stack: process.env.NODE_ENV === "production" ? "🍰" : err.stack,
-    });
-  }
-
-  next();
-
-  // res.status(statusCode).res.json({
-  //   message,
-  //   stack: process.env.NODE_ENV === "production" ? "🍰" : err.stack,
-  // });
+  const message = err.message || "Resource not found";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🍰" : err.stack,
+  });
 };
 
 //exporting multiple functions
--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ connectDB();
 
 const app = express();
 
-// app.use(express.json());
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
