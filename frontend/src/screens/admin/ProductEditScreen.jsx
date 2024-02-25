7f4b354f8d4ef4a2c1ef0683beacd07e47ee6276
Serkans26 <s.safran84@gmail.com>
2024-02-25 13:51:48 +0100

    Deploy

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -103,6 +103,7 @@ const ProductEditScreen = () => {
       <FormContainer>
         <h1>Edit Product</h1>
         {loadingUpdate && <Loader />}
+        {loadingUpload && <Loader />}
 
         {isLoading ? (
           <Loader />
